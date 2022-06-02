fcf4d468b5c3c06fafdfc73363b999c39fe9621b
Nurzod <nurzodbekmardiyev1306@gmail.com>
2022-06-02 13:44:58 +0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,58 +1,60 @@
-"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-// Bismilloh
+  // Bismilloh
 
-// Languge hover display block
-const language = document.querySelector("[lang_in]"),
-  activeLang = document.querySelector("[activeLang]");
+  // Languge hover display block
+  const languages = document.querySelector("[lang_in]"),
+    activeLang = document.querySelector("[activeLang]");
 
-language.classList.add("hide");
-function showMyLang() {
-  language.classList.add("show");
-  language.classList.remove("hide");
-}
+  activeLang.addEventListener("click", openModal);
 
-function hideMyLang() {
-  language.classList.add("hide");
-  language.classList.remove("show");
-}
-
-activeLang.addEventListener("click", () => {
-  console.log("Hi");
-  showMyLang();
-});
-activeLang.addEventListener("click", (e) => {
-  if (e.target === activeLang) {
-    hideMyLang();
+  function openModal() {
+    languages.classList.add("show");
+    languages.classList.remove("hide");
+    document.body.style.overflow = "hidden";
+    // clearInterval(modalTimer);
   }
+
+  function closeModal() {
+    languages.classList.add("hide");
+    languages.classList.remove("show");
+    document.body.style.overflow = "";
+  }
+
+  activeLang.addEventListener("click", (e) => {
+    if (e.target === activeLang) {
+      closeModal();
+    }
+  });
+
+  // Navbar
+
+  const menuBtn = document.querySelector(".menu-icons"),
+    navbar = document.querySelector("[navbar]");
+
+  menuBtn.addEventListener("click", () => {
+    navbar.style.display = "none";
+    console.log("hi");
+  });
+
+  // const car = {
+  //   color: "white",
+  //   shum: 100,
+  //   name: "mers",
+  //   extra: {
+  //     isBuy: true,
+  //     isAirBag: false,
+  //   },
+  // };
+
+  // let str = JSON.stringify(car);
+  // const clone = JSON.parse(str);
+  // clone.extra.isAirBag = true;
+  // console.log(car);
+  // console.log(clone);
+
+  const next = document.querySelector(".fa-angle-left"),
+    prev = document.querySelector(".fa-angle-right"),
+    advertisement1 = document.querySelector("[data-advertisement2]");
 });
-
-// Navbar
-
-const menuBtn = document.querySelector(".menu-icons"),
-  navbar = document.querySelector("[navbar]");
-
-menuBtn.addEventListener("click", () => {
-  navbar.style.display = "none";
-  console.log("hi");
-});
-
-// const car = {
-//   color: "white",
-//   shum: 100,
-//   name: "mers",
-//   extra: {
-//     isBuy: true,
-//     isAirBag: false,
-//   },
-// };
-
-// let str = JSON.stringify(car);
-// const clone = JSON.parse(str);
-// clone.extra.isAirBag = true;
-// console.log(car);
-// console.log(clone);
-
-const next = document.querySelector(".fa-angle-left"),
-  prev = document.querySelector(".fa-angle-right"),
-  advertisement1 = document.querySelector("[data-advertisement2]");
